3ed3f6f4e99f11aebf4fb027d05763a64a3f1b95
SungHeeCheol <hhhc0213@naver.com>
2022-12-09 13:01:19 +0900

    12/09

--- db_study.sql
+++ db_study.sql
@@ -20,30 +20,40 @@ DROP DATABASE IF EXISTS `db_study`;
 CREATE DATABASE IF NOT EXISTS `db_study` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `db_study`;
 
+-- 테이블 db_study.order_dtl 구조 내보내기
+DROP TABLE IF EXISTS `order_dtl`;
+CREATE TABLE IF NOT EXISTS `order_dtl` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order_id` int NOT NULL DEFAULT '0',
+  `prorder_id` int NOT NULL DEFAULT '0',
+  `order_stock` int NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study.order_dtl:~0 rows (대략적) 내보내기
+DELETE FROM `order_dtl`;
+INSERT INTO `order_dtl` (`id`, `order_id`, `prorder_id`, `order_stock`) VALUES
+	(1, 1, 1, 3),
+	(2, 1, 2, 10);
+
 -- 테이블 db_study.order_mst 구조 내보내기
 DROP TABLE IF EXISTS `order_mst`;
 CREATE TABLE IF NOT EXISTS `order_mst` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `product_id` int NOT NULL DEFAULT '0',
-  `order_stock` int NOT NULL DEFAULT '0',
   `user_id` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- 테이블 데이터 db_study.order_mst:~5 rows (대략적) 내보내기
+-- 테이블 데이터 db_study.order_mst:~1 rows (대략적) 내보내기
 DELETE FROM `order_mst`;
-INSERT INTO `order_mst` (`id`, `product_id`, `order_stock`, `user_id`) VALUES
-	(1, 1, 3, 1),
-	(2, 2, 1, 1),
-	(3, 1, 2, 2),
-	(4, 2, 4, 2),
-	(5, 2, 1, 3);
+INSERT INTO `order_mst` (`id`, `user_id`) VALUES
+	(1, 2);
 
 -- 테이블 db_study.product_mst 구조 내보내기
 DROP TABLE IF EXISTS `product_mst`;
 CREATE TABLE IF NOT EXISTS `product_mst` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `product_name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `product_name` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `product_price` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `product_name` (`product_name`)
@@ -69,37 +79,106 @@ CREATE TABLE IF NOT EXISTS `student_mst` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- 테이블 데이터 db_study.student_mst:~11 rows (대략적) 내보내기
+-- 테이블 데이터 db_study.student_mst:~9 rows (대략적) 내보내기
 DELETE FROM `student_mst`;
 INSERT INTO `student_mst` (`id`, `name`, `student_year`, `score`) VALUES
 	(1, '임지현', 1, 80),
-	(2, '신경수', 2, 90),
-	(3, '고희주', 3, 75),
+	(2, '신경수', 2, 100),
 	(4, '문승주', 1, 85),
 	(5, '장건녕', 2, 95),
 	(6, '박수현', 3, 90),
 	(7, '황석민', 1, 80),
-	(8, '박준현', 2, 85),
+	(8, '박준현', 2, 100),
 	(9, '이승아', 3, 90),
-	(10, '김수현', 1, 75),
 	(12, NULL, 1, 100);
+
+-- 테이블 db_study.study_notice_img 구조 내보내기
+DROP TABLE IF EXISTS `study_notice_img`;
+CREATE TABLE IF NOT EXISTS `study_notice_img` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `notice_id` int DEFAULT NULL,
+  `img_name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study.study_notice_img:~7 rows (대략적) 내보내기
+DELETE FROM `study_notice_img`;
+INSERT INTO `study_notice_img` (`id`, `notice_id`, `img_name`) VALUES
+	(1, 1, 'a.png'),
+	(2, 1, 'b.png'),
+	(3, 2, 'c.png'),
+	(4, 2, 'd.png'),
+	(5, 2, 'e.png'),
+	(6, 3, 'f.png'),
+	(7, 3, 'g.png');
+
+-- 테이블 db_study.study_notice_mst 구조 내보내기
+DROP TABLE IF EXISTS `study_notice_mst`;
+CREATE TABLE IF NOT EXISTS `study_notice_mst` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `content` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `writer_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study.study_notice_mst:~0 rows (대략적) 내보내기
+DELETE FROM `study_notice_mst`;
+INSERT INTO `study_notice_mst` (`id`, `title`, `content`, `writer_id`) VALUES
+	(1, '첫번째 공지사항', 'aaa', 1),
+	(2, '두번째 공지사항', 'bbb', 1),
+	(3, '세번째 공지사항', 'ccc', 2);
+
+-- 테이블 db_study.study_writer_mst 구조 내보내기
+DROP TABLE IF EXISTS `study_writer_mst`;
+CREATE TABLE IF NOT EXISTS `study_writer_mst` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study.study_writer_mst:~0 rows (대략적) 내보내기
+DELETE FROM `study_writer_mst`;
+INSERT INTO `study_writer_mst` (`id`, `name`) VALUES
+	(1, '김준일'),
+	(2, '김준이');
+
+-- 테이블 db_study.user_dtl 구조 내보내기
+DROP TABLE IF EXISTS `user_dtl`;
+CREATE TABLE IF NOT EXISTS `user_dtl` (
+  `id` int NOT NULL,
+  `phone` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `address` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- 테이블 데이터 db_study.user_dtl:~0 rows (대략적) 내보내기
+DELETE FROM `user_dtl`;
+INSERT INTO `user_dtl` (`id`, `phone`, `address`, `create_date`, `update_date`) VALUES
+	(2, NULL, NULL, '2022-12-09 10:53:42', '2022-12-09 10:53:42'),
+	(3, NULL, NULL, '2022-12-09 10:53:42', '2022-12-09 10:53:42');
 
 -- 테이블 db_study.user_mst 구조 내보내기
 DROP TABLE IF EXISTS `user_mst`;
 CREATE TABLE IF NOT EXISTS `user_mst` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `password` varchar(50) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `create_date` datetime NOT NULL,
+  `update_date` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- 테이블 데이터 db_study.user_mst:~3 rows (대략적) 내보내기
+-- 테이블 데이터 db_study.user_mst:~2 rows (대략적) 내보내기
 DELETE FROM `user_mst`;
-INSERT INTO `user_mst` (`id`, `username`, `password`) VALUES
-	(1, 'aaa', '1111'),
-	(2, 'bbb', '2222'),
-	(3, 'ccc', '3333');
+INSERT INTO `user_mst` (`id`, `username`, `password`, `name`, `email`, `create_date`, `update_date`) VALUES
+	(2, 'ccc', '1234', '김준이', 'ccc@gmail.com', '2022-12-09 10:53:42', '2022-12-09 10:53:42'),
+	(3, 'bbb', '1234', '김준이', 'bbb@gmail.com', '2022-12-09 10:53:42', '2022-12-09 10:53:42');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
