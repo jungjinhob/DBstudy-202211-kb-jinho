cf401a5022c1eb0d7a666cfd1f2bfbffb6aa6e5f
SungHeeCheol <hhhc0213@naver.com>
2022-12-09 13:02:45 +0900

    12/09

---
+++ sql3.sql
@@ -0,0 +1,36 @@
+INSERT INTO
+	user_mst
+VALUES
+	(0,'aaa','1234','김준이','aaa@gmail.com',NOW(),NOW()),
+	(0,'ccc','1234','김준이','ccc@gmail.com',NOW(),NOW()),
+	(0,'bbb','1234','김준이','bbb@gmail.com',NOW(),NOW());
+	
+DELETE
+FROM
+	user_mst 
+WHERE 
+	id = 1;
+
+
+SELECT *
+
+FROM
+	order_mst om
+	LEFT OUTER JOIN order_dtl od ON(od.order_id = om.id),
+	LEFT OUTER JOIN product_mst pm ON(pm.id = od.proder_id)
+	LEFT OUTER JOIN user_mst um ON(om.user_id = um.id)
+
+ORDER BY 
+	od.id;
+	
+
+SELECT
+	
+	nm.title,
+	nm.content,
+	wm.name,
+	ni.img_name
+FROM
+	study_notice_mst nm
+	LEFT OUTER JOIN study_write_mst wm ON(wm.id = nm.writer_id)
+	LEFT OUTER JOIN study_notice_img ni ON(ni.notice_id = nm.id);
